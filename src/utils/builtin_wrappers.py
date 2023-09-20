f7b286041e2b4f320bade454ec248e98e5f79bb7
p6rdcn <p6rdcn@hallgato.uni-neumann.hu>
2023-09-20 17:02:57 +0200

    2023.09.20 python hazi 1 megoldas

--- src/utils/builtin_wrappers.py
+++ src/utils/builtin_wrappers.py
@@ -7,8 +7,6 @@ def number_of_elements_in_list(input_list):
 def remove_every_element_from_list(input_list):
     input_list.clear()
 
-
-
 def reverse_list(input_list):
     return list(reversed(input_list))
 
