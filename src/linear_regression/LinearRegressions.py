0171f9ffc092106f0db23d71250620fdb67395a5
p6rdcn <p6rdcn@hallgato.uni-neumann.hu>
2023-10-29 22:08:32 +0100

    My modified solution for Week6 test (P6RDCN)

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -27,18 +27,14 @@ class LinearRegressionSM:
         p_vals_df = pd.DataFrame({"P-values for the corresponding coefficients": p_vals})
         return p_vals_df.squeeze()
 
-    def get_wald_test_result(self):
+    def get_wald_test_result(self, restriction_matrix):
+        test = self.model.wald_test(restriction_matrix)
         fvalue = self.model.fvalue
         pvalue = self.model.f_pvalue
-
-        print("F-value:", round(fvalue, 3))
-        print("p-value:", round(pvalue, 3))
+        return f"F-value: {fvalue:.2f}, p-value: {pvalue:.3f}"
 
     def get_model_goodness_values(self):
         ars = self.model.rsquared_adj
         ak = self.model.aic
         by = self.model.bic
-
-        print("Adjusted R-squared:", round(ars, 3))
-        print("AIC:", round(ak, 3))
-        print("BIC:", round(by, 3))
+        return f"Adjusted R-squared: {ars:.3f}, Akaike IC: {ak:.2e}, Bayes IC: {by:.2e}"
