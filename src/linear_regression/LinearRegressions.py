0b2e6f449e631b809223aa1f4de5dfc9bcdd42f9
p6rdcn <p6rdcn@hallgato.uni-neumann.hu>
2023-11-15 20:32:16 +0100

    The LinearRegressionGLS class was added for the Week8 test

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -84,3 +84,62 @@ class LinearRegressionNP:
         crs = 1 - self.residuals.T @ self.residuals / (y_centered.T @ y_centered)
         ars = 1 - (1 - crs) * (self.n - 1) / self.dof
         return f"Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}"
+
+
+class LinearRegressionGLS:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
+
+        self.n = self.right_hand_side.shape[0]
+        self.X = np.concatenate((np.ones((self.n, 1)), self.right_hand_side), axis = 1)
+        self.k = self.X.shape[1]
+        self.dof = self.n - self.k
+
+        self.gls_betas = None
+        self.v_inv = None
+        # self.gls_X = None
+        # self.gls_y = None
+        self.gls_residuals = None
+        self.gls_variance = None
+        self.gls_SE = None
+
+    def fit(self):
+        ols_betas = np.linalg.inv(self.X.T @ self.X) @ self.X.T @ self.left_hand_side
+        ols_resid = self.left_hand_side - self.X @ ols_betas
+        y = np.log(ols_resid ** 2)
+        log_betas = np.linalg.inv(self.X.T @ self.X) @ self.X.T @ y
+        predictions = np.sqrt(np.exp(self.X @ log_betas))
+        self.v_inv = np.diag(1.0 / predictions)
+        self.gls_betas = np.linalg.inv(self.X.T @ self.v_inv @ self.X) @ self.X.T @ self.v_inv @ self.left_hand_side
+        # c = np.linalg.cholesky(self.v_inv)
+        # self.gls_X = c @ self.X
+        # self.gls_y = c @ self.left_hand_side
+
+    def get_params(self):
+        return pd.Series(self.gls_betas, name = "Beta coefficients")
+
+    def get_pvalues(self):
+        self.gls_residuals = self.left_hand_side - self.X @ self.gls_betas
+        self.gls_variance = self.gls_residuals.T @ self.gls_residuals / self.dof
+        self.gls_SE = np.sqrt(np.diag(self.gls_variance * np.linalg.inv(self.X.T @ self.v_inv @ self.X)))
+        t_values = self.gls_betas / self.gls_SE
+        p_values = 2 * (1 - stats.t.cdf(abs(t_values), self.dof))
+        return pd.Series(p_values, name = "P-values for the corresponding coefficients")
+
+    def get_wald_test_result(self, constraints):
+        R = np.asmatrix(constraints)
+        m = R.shape[0]
+        RBr = R @ self.gls_betas
+        RXTXRT = R @ np.linalg.inv(self.X.T @ self.v_inv @ self.X) @ R.T
+        wald_value = RBr @ np.linalg.inv(RXTXRT) @ RBr.T / m / self.gls_variance
+        p_value = 1 - stats.f.cdf(wald_value.item(), m, self.dof)
+        return f"Wald: {wald_value.item():.3f}, p-value: {p_value:.3f}"
+
+    def get_model_goodness_values(self):
+        SST = self.left_hand_side.T @ self.v_inv @ self.left_hand_side
+        SSR = self.left_hand_side.T @ self.v_inv @ self.X @ np.linalg.inv(
+            self.X.T @ self.v_inv @ self.X) @ self.X.T @ self.v_inv @ self.left_hand_side
+        crs = 1 - SSR / SST
+        ars = 1 - (1 - crs) * (self.n - 1) / self.dof
+        return f"Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}"
